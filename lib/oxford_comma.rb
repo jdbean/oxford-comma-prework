66bd287130e2415e4c0ea4558c82407d12604e34
J.D. Bean <jonathandbean@gmail.com>
2018-02-15 22:31:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 def oxford_comma(array)
-  string = array.join(", "")
-  array = string.split(",")
   array.insert(-1, "and #{array[-1]}")
-  oxford_string = array.join
-  return array
+  string = array.join(", ")
+  return string
 end
